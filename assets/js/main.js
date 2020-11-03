b5114f55c5eef74551e82d0e9b24b89a39d48787
Ishan[KBG] <71317082+LunaeStudios@users.noreply.github.com>
2020-11-03 20:39:11 +0530

    added preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-
+//Preloader Starts
+         // Main function
+         function preloaderLoad() {
+           console.log("Lol");
+           document.getElementById("preloader").style.display = "";
+         };
+         
+         // On Load event
+         window.onload = function() {
+           preloaderLoad();
+         };
+         
+         // Clear the preloader
+         setTimeout(() => {
+           console.log("End");
+           document.getElementById("preloader").style.display = "none";
+         }, 10000 /* Change time here */);
+         //Preloader Ends
